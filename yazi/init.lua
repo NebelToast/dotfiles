52dcec62339362ba29642d09244185eee0222284
NebelToast <juliussteude@gmail.com>
2025-11-24 21:28:40 +0100

    add: smart-enter plugin

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,3 +1,15 @@
 require("full-border"):setup {
 	type = ui.Border.ROUNDED,
 }
+
+function Header:render(area)
+	return {}
+end
+
+function Parent:render(area)
+	return {
+		ui.List(self._folder.window, self._folder:layout())
+			:area(area)
+			:style(ui.Style():fg("#9D949F")),
+	}
+end
