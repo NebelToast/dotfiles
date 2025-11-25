a69db1a425e8f2870569c8e5e330219a3b8d3357
NebelToast <juliussteude@gmail.com>
2025-11-25 09:17:39 +0100

    get rid of alias for dooit

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,6 @@ end
 zoxide init fish | source
 alias cd="z"
 alias darkrei="brrtfetch -width 60 -fps 17 -multiplier 10 -offset 3 /home/julius/gifs/darkrai.gif"
-alias dooit=' nix-shell /home/julius/.config/dooit/ --run "dooit"'
 alias sync='rclone bisync "/home/julius/Videos" "gdrive:Videos" -v'
 
 
